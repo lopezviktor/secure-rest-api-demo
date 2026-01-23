5bc00d653de97ed4f03d577cb269aeed17fa533e
Victor <victorlopezlopez93@gmail.com>
2026-01-23 18:05:22 +0000

    chore: bootstrap Spring Boot project

---
+++ src/main/java/io/viktor/backend/SecureRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package io.viktor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureRestApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureRestApiDemoApplication.class, args);
+    }
+
+}
